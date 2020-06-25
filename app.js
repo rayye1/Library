2aab1d7ef39b95d984ad7bec02949b67e5c97c6d
Ray Ye <yer@purdue.edu>
2020-06-25 11:21:42 -0400

    Add Books Button Functionality

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ function bookObj(title, author, pages, haveRead) {
   this.haveRead = haveRead
 }
 
+function insertBook() {
+  let table = document.getElementById("myTable");
+  let row = table.insertRow(2)
+  let cell1 = row.insertCell(0)
+  let cell2 = row.insertCell(1)
+  let cell3 = row.insertCell(2)
+  
+  cell1.innerHTML = "NEW C2LL1";
+  cell2.innerHTML = "NEW CELL2";
+}
+
 function addLibrary(book) {
   library.append(book)
 }
